1df43743ae9525f0dd57a6325b82242eec73d12d
Yomanbest <106496623+Yomanbest67@users.noreply.github.com>
2024-03-22 16:22:17 +0000

    Added CV details area

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import '../styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div class="CV">
+    <h2>This is where the CV goes!</h2>
+    <div class="CV-display">
+
+
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+import '../styles/SideMenu.css'
+
+export default function SideMenu () {
+    return (
+        <div className='sideMenu'>
+            <div className='sideHeader'>
+                <h2>Edit details here.</h2>
+            </div>
+
+            <div className='personalInfo'>
+                
+                <h3>Personal Info</h3>
+
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id='firstName' />
+                </div>
+
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id='lastName' />
+                </div>
+
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id='email' />
+                </div>
+
+                <div>
+                    <label htmlFor="number">Phone Number</label>
+                    <input type="tel" id='number' pattern="\d*"/>
+                </div>
+            </div>
+
+            <div className='education'>
+                <h3>Education</h3>
+
+                <div>
+                    <label htmlFor="school">School Name</label>
+                    <input type="text" id='school' />
+                </div>
+
+                <div>
+                    <label htmlFor="study">Title of Study</label>
+                    <input type="text" id='study' />
+                </div>
+
+                <div>
+                    <label htmlFor="date">Date of Study</label>
+                    <input type="text" id='date' />
+                </div>
+            </div>
+
+            <div className='experience'>
+                <h3>Experience</h3>
+
+                <div>
+                    <label htmlFor="company">Company Name</label>
+                    <input type="text" id='company' />
+                </div>
+
+                <div>
+                    <label htmlFor="position">Position Title</label>
+                    <input type="text" id='position' />
+                </div>
+
+                <div>
+                    <label htmlFor="responsibility">Main Responsibilities</label>
+                    <textarea id='responsibility' />
+                </div>
+
+                <div>
+                    <label htmlFor="dateFrom">Start Date</label>
+                    <input type="text" id='dateFrom' />
+                </div>
+
+                <div>
+                    <label htmlFor="dateTo">End Date</label>
+                    <input type="text" id='dateTo' />
+                </div>
+            </div>
+        </div>
+    )
+}
