406e50b82b7c656f1c49553b959f6bf012728417
Yomanbest <106496623+Yomanbest67@users.noreply.github.com>
2024-03-22 19:44:53 +0000

    Added event handler for input changes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+import '../styles/SideMenu.css'
 import '../styles/App.css'
+import '../styles/index.css'
 
 function App() {
   const [info, setInfo] = useState({
@@ -17,28 +19,105 @@ function App() {
     dateTo: "06/2022"
   });
 
+  const updateInfo = (event) => {
+    setInfo({ ...info, [event.target.name]: event.target.value });
+  };
+
   return (
-    <div class="CV">
-      <h2>Preview Resume</h2>
-      <div class="CV-display">
-        <div className='cvPersonal'>
-          <h2>{info.firstName} {info.lastName}</h2>
-          <h3>{info.email} | {info.phone}</h3>
-        </div>
+    <div className='app'>
+      <div className='sideMenu'>
+              <div className='sideHeader'>
+                  <h2>Edit details here.</h2>
+              </div>
 
-        <div className='cvEducation'>
-          <h2>Education</h2>
-          <h4>{info.school}</h4>
-          <h5>{info.degree}</h5>
-          <h5>{info.studyDate}</h5>
-        </div>
+              <div className="sideInfo">
+                  <div className='personalInfo'>
+                  
+                      <h3>Personal Info</h3>
+                      <div>
+                          <label htmlFor="firstName">First Name</label>
+                          <input type="text" name='firstName' onChange={updateInfo} id='firstName' value={info.firstName}/>
+                      </div>
+                      <div>
+                          <label htmlFor="lastName">Last Name</label>
+                          <input type="text" id='lastName' name='lastName' onChange={updateInfo} value={info.lastName}/>
+                      </div>
+                      <div>
+                          <label htmlFor="email">Email</label>
+                          <input type="email" id='email' name='email' onChange={updateInfo} value={info.email}/>
+                      </div>
+                      <div>
+                          <label htmlFor="number">Phone Number</label>
+                          <input type="tel" id='number' name='phone' onChange={updateInfo} value={info.phone} pattern="\d*"/>
+                      </div>
+                  </div>
+                  <div className='education'>
+                      <h3>Education</h3>
+                      <div>
+                          <label htmlFor="school">School Name</label>
+                          <input type="text" id='school' name='school' onChange={updateInfo} value={info.school} />
+                      </div>
+                      <div>
+                          <label htmlFor="study">Title of Study</label>
+                          <input type="text" id='study' name='degree' onChange={updateInfo} value={info.degree}/>
+                      </div>
+                      <div>
+                          <label htmlFor="date">Date of Study</label>
+                          <input type="text" id='date' name='studyDate' onChange={updateInfo} value={info.studyDate}/>
+                      </div>
+                  </div>
+                  <div className='experience'>
+                      <h3>Experience</h3>
+                      <div>
+                          <label htmlFor="company">Company Name</label>
+                          <input type="text" id='company' name='companyName' onChange={updateInfo} value={info.companyName}/>
+                      </div>
+                      <div>
+                          <label htmlFor="position">Position Title</label>
+                          <input type="text" id='position' name='position' onChange={updateInfo} value={info.position} />
+                      </div>
+                      <div>
+                          <label htmlFor="responsibility">Main Responsibilities</label>
+                          <textarea id='responsibility' name='responsibility' onChange={updateInfo} value={info.responsibility}/>
+                      </div>
+                      <div>
+                          <label htmlFor="dateFrom">Start Date</label>
+                          <input type="text" id='dateFrom' name='dateFrom' onChange={updateInfo} value={info.dateFrom}/>
+                      </div>
+                      <div>
+                          <label htmlFor="dateTo">End Date</label>
+                          <input type="text" id='dateTo' name='dateTo' onChange={updateInfo} value={info.dateTo} />
+                      </div>
+                  </div>
 
-        <div className='cvExperience'>
-          <h2>Experience</h2>
-          <h3>{info.companyName}</h3>
-          <h4>{info.position}</h4>
-          <h5>From: {info.dateFrom} To: {info.dateTo}</h5>
-          <p>{info.responsibility}</p>
+                  <div className='buttons'>
+                      <button className='editBtn'>Edit</button>
+                      <button className='submitBtn'>Submit</button>
+                  </div>
+              </div>
+          </div>
+      <div class="CV">
+        <h2>Preview Resume</h2>
+        <div class="CV-display">
+          <div className='cvPersonal'>
+            <h2>{info.firstName} {info.lastName}</h2>
+            <h3>{info.email} | {info.phone}</h3>
+          </div>
+
+          <div className='cvEducation'>
+            <h2>Education</h2>
+            <h4>{info.school}</h4>
+            <h5>{info.degree}</h5>
+            <h5>{info.studyDate}</h5>
+          </div>
+
+          <div className='cvExperience'>
+            <h2>Experience</h2>
+            <h3>{info.companyName}</h3>
+            <h4>{info.position}</h4>
+            <h5>From: {info.dateFrom} To: {info.dateTo}</h5>
+            <p>{info.responsibility}</p>
+          </div>
         </div>
       </div>
     </div>
