e967e4e4c965590ce1e0bc34018d4edf56c6cdfa
Yomanbest <106496623+Yomanbest67@users.noreply.github.com>
2024-03-23 21:44:32 +0000

    Changed data structure.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,47 +4,44 @@ import '../styles/App.css'
 import '../styles/index.css'
 
 function App() {
-  const [info, setInfo] = useState({
-    'First Name': "John",
-    'Last Name': "Doe",
-    'Email': "johndoe@notarealemail.com",
-    'Phone': "078230599966",
+  const [cvData, setCvData] = useState({
+    info: {
+      'First Name': "John",
+      'Last Name': "Doe",
+      'Email': "johndoe@notarealemail.com",
+      'Phone': "078230599966",
+    },
+    education: [
+      {
+        'School': "University of California",
+        'Degree': "BA in Arts",
+        'Date of Study': "05/2019 - 05/2022",
+      }
+    ],
+    experience: [
+      {
+        'Name of Company': "Hola Cider",
+        'Title': "Operation Engineer",
+        'Responsibilites': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis turpis ac est fermentum ultrices. Vestibulum rhoncus odio sagittis, faucibus erat id, consectetur metus. Sed ornare vitae eros fermentum dapibus. Cras aliquam urna ipsum, eu consequat diam pulvinar eget. Praesent et dolor viverra, finibus arcu ultricies, ultricies magna. Nunc erat sapien, tincidunt eu mauris et, pretium facilisis lectus.",
+        'Date From': "05/2019",
+        'Date To': "06/2022"
+      },
+    ]
   });
 
-  const [education, setEducation] = useState({
-      'School': "University of California",
-      'Degree': "BA in Arts",
-      'Date of Study': "05/2019 - 05/2022",
-  })
-
-  const [experience, setExperience] = useState({
-      'Name of Company': "Hola Cider",
-      'Title': "Operation Engineer",
-      'Responsibilites': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis turpis ac est fermentum ultrices. Vestibulum rhoncus odio sagittis, faucibus erat id, consectetur metus. Sed ornare vitae eros fermentum dapibus. Cras aliquam urna ipsum, eu consequat diam pulvinar eget. Praesent et dolor viverra, finibus arcu ultricies, ultricies magna. Nunc erat sapien, tincidunt eu mauris et, pretium facilisis lectus.",
-      'Date From': "05/2019",
-      'Date To': "06/2022"
-  })
-
-
-  const updateInfo = (event) => {
-    setInfo({ ...info, [event.target.name]: event.target.value });
+  // Pass field name to updateCvData and update based on that.
+  const updateCvData = (event) => {
+    setCvData({ ...cvData, [event.target.name]: event.target.value });
   };
 
-  const updateEducation = (event) => {
-    setEducation({ ...education, [event.target.name]: event.target.value });
-  }
 
-  const updateExperience = (event) => {
-    setExperience({ ...experience, [event.target.name]: event.target.value });
-  }
-
-  function generateSideInfo(object, onUpdate) {
+  function generateSideInfo(object, field) {
     return (
       <div>
         {Object.entries(object).map(([key, value]) => (
           <>
             <label htmlFor={key}>{key}</label>
-            <input type="text" id={key} name={key} value={value} onChange={(e) => onUpdate(e)}/>
+            <input type="text" className={field} id={key} name={key} value={value} onChange={updateCvData}/>
           </>
         ))}
       </div>
@@ -61,28 +58,21 @@ function App() {
 
               <div className="sideInfo">
                 <div className='personalInfo'>
-                  <h2>Personal Info</h2>
-                  {generateSideInfo(info, updateInfo)}
+                  {generateSideInfo(cvData['info'], 'info')}
                 </div>
                  
                 <div className='education'>
                   <h2>Education</h2>
-                  {generateSideInfo(education, updateEducation)}
-                  
-                  {/* <div className='buttons'>
-                    <button onClick={}>Add</button>
-                    <button>Delete</button>
-                  </div> */}
+                  {generateSideInfo(cvData['education'][0], 'education')}
+                  <button>Add</button>
+                  <button>Remove</button>
                 </div>
 
                 <div className='experience'>
                   <h2>Experience</h2>
-                  {generateSideInfo(experience, updateExperience)}
-
-                  {/* <div className='buttons'>
-                    <button>Add</button>
-                    <button>Delete</button>
-                  </div> */}
+                  {generateSideInfo(cvData['experience'][0], 'experience')}
+                  <button>Add</button>
+                  <button>Remove</button>
                 </div>
               </div>
 
@@ -91,23 +81,31 @@ function App() {
         <h2>Preview Resume</h2>
         <div class="CV-display">
           <div className='cvPersonal'>
-            <h2>{info['First Name']} {info['Last Name']}</h2>
-            <h3>{info['Email']} | {info['Phone']}</h3>
+            <h2>{cvData['info']['First Name']} {cvData['info']['Last Name']}</h2>
+            <h3>{cvData['info']['Email']} | {cvData['info']['Phone']}</h3>
           </div>
 
           <div className='cvEducation'>
             <h2>Education</h2>
-            <h4>{education['School']}</h4>
-            <h5>{education['Degree']}</h5>
-            <h5>{education['Date of Study']}</h5>
+            {cvData.education.map((entry, index) => (
+              <div key={index} className='education-entry'>
+                <h3>{entry['School']}</h3>
+                <h4>{entry['Degree']}</h4>
+                <h5>{entry['Date of Study']}</h5>
+              </div>
+            ))}
           </div>
 
           <div className='cvExperience'>
             <h2>Experience</h2>
-            <h3>{experience['Name of Company']}</h3>
-            <h4>{experience['Title']}</h4>
-            <h5>From: {experience['Date From']} To: {experience['Date To']}</h5>
-            <p>{experience['Responsibilites']}</p>
+              {cvData.experience.map((entry, index) => (
+                    <div key={index} className='work-entry'>
+                      <h3>{entry['Name of Company']}</h3>
+                      <h4>{entry['Title']}</h4>
+                      <h5>From: {entry['Date From']} To: {entry['Date To']}</h5>
+                      <p>{entry['Responsibilites']}</p>
+                    </div>
+              ))}
           </div>
         </div>
       </div>
